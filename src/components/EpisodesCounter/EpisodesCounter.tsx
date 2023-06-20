ab73058e33f9a52d8f78092bafa02bacba26705d
Petar Tsenev <petar.tzenev@gmail.com>
2023-06-20 20:05:21 +0200

    feat: add EpisodesCounter

---
+++ src/components/EpisodesCounter/EpisodesCounter.tsx
@@ -0,0 +1,15 @@
+import { IEpisodeProps } from '../../types/podcast.types';
+import './EpisodesCounter.scss';
+
+const EpisodesCounter = (props: { data: IEpisodeProps[] }) => {
+  const { data } = props;
+
+  return (
+    <div className="episodes-card">
+      <strong>Episodes: </strong>
+      <span>{data && data.length}</span>
+    </div>
+  );
+};
+
+export default EpisodesCounter;
