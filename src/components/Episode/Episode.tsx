cab620b9a51c2e6f518b1a4f97472f79e60309e2
Petar Tsenev <petar.tzenev@gmail.com>
2023-06-21 01:08:59 +0200

    fix(styling): activate anchors in episode desc

--- src/components/Episode/Episode.tsx
+++ src/components/Episode/Episode.tsx
@@ -4,10 +4,36 @@ import './Episode.scss';
 const Episode = () => {
   const { selectedEpisode } = usePodcasts();
 
+  const linkRegex = /(https?\:\/\/)?(www\.)?[^\s]+\.[^\s]+/g;
+  const replacer = (matched: string) => {
+    let withProtocol = matched;
+
+    if (!withProtocol.startsWith('http')) {
+      withProtocol = 'http://' + matched;
+    }
+
+    const newStr = `<a
+      class="text-link"
+      href="${withProtocol}"
+    >
+      ${matched}
+    </a>`;
+
+    return newStr;
+  };
+
+  const modifiedStr =
+    selectedEpisode &&
+    selectedEpisode?.description.replace(linkRegex, replacer);
+  const target = document.getElementById('target');
+  if (target !== undefined && target !== null && modifiedStr !== undefined) {
+    target.innerHTML = modifiedStr;
+  }
+
   return selectedEpisode ? (
     <div className="episode episode-card">
       <div className="episode__title">{selectedEpisode.title}</div>
-      <div className="episode__description">{selectedEpisode.description}</div>
+      <div id="target" className="episode__description"></div>
       <div className="audioControl">
         <audio
           id={selectedEpisode.id.toString()}
